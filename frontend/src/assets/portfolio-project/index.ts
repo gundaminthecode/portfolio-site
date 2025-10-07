4a9585f08ca09cc6c0f495d8593233ce26d040d2
gundaminthecode <nmath2211@outlook.com>
2025-10-07 12:18:56 +1000

    Begun Project Discussion Page

---
+++ frontend/src/assets/portfolio-project/index.ts
@@ -0,0 +1,16 @@
+// Update the filenames/extensions below to match your actual files.
+
+export { default as moodBoardImage } from "./mood-board.png";
+export { default as wireframe1 } from "./wireframe1.png";
+export { default as wireframe2 } from "./wireframe2.png";
+
+export { default as potStyleGuideImage1 } from "./Logo.png";
+export { default as potStyleGuideImage2 } from "./Colors (1).png";
+
+export { default as pitchOutcomeImageMobile1 } from "./nickmathiasen.dev_Homepage(iPhone 14 Pro Max).png";
+export { default as pitchOutcomeImageMobile2 } from "./nickmathiasen.dev_Projects(iPhone 14 Pro Max).png";
+export { default as pitchOutcomeImageDesktop1 } from "./nickmathiasen.dev_HomepageDesktop.png";
+export { default as pitchOutcomeImageDesktop2 } from "./nickmathiasen.dev_ProjectsDesktop.png";
+
+export { default as initialRedesignImage } from "./intital-redesign.png";
+export { default as refinementConcepts } from "./new-wireframe.png";
--- frontend/src/pages/PortfolioProject.tsx
+++ frontend/src/pages/PortfolioProject.tsx
@@ -1,5 +1,21 @@
 import { Link } from 'react-router-dom';
 import useScrollReveal from '../hooks/useScrollReveal';
+import "../styles/portfolio-project-page.css"
+
+import {
+    moodBoardImage,
+    wireframe1,
+    wireframe2,
+    potStyleGuideImage1,
+    potStyleGuideImage2,
+    pitchOutcomeImageMobile1,
+    pitchOutcomeImageMobile2,
+    pitchOutcomeImageDesktop1,
+    pitchOutcomeImageDesktop2,
+    initialRedesignImage,
+    refinementConcepts
+} from '../assets/portfolio-project/index.ts';
+
 
 export default function PortfolioProject() {
     useScrollReveal();
@@ -8,32 +24,59 @@ export default function PortfolioProject() {
     return (
         <div id='content-stack'>
             <Link to="/" className='back-link'>Back</Link>
-            <div className='content-slice'>
+            <div className='content-slice portfolio-info-slices'>
                 <div className='slice-content'>
                     <h1>Custom Developed Web-Portfolio</h1>
                 </div>
             </div>
-            <div className='content-slice'>
+            <div className='content-slice portfolio-info-slices'>
                 <div className='slice-content'>
                     <h1>About This Project</h1>
                 </div>
             </div>
-            <div className='content-slice'>
+            <div className='content-slice portfolio-info-slices'>
                 <div className='slice-content'>
                     <h1>Development</h1>
+                    <div className='development-slice'>
+                        <h2>Mood Board</h2>
+                            <img src={moodBoardImage} alt="Mood Board" />
+                    </div>
+                    <div className='development-slice'>
+                        <h2>Concepts</h2>
+                            <h3>Initial Wireframes</h3>
+                                <img src={wireframe1} alt="Wireframe 1" />
+                                <img src={wireframe2} alt="Wireframe 2" />
+                            <h3>Potential Styleguide</h3>
+                                <img src={potStyleGuideImage1} alt="Potential Styleguide" />
+                                <img src={potStyleGuideImage2} alt="Potential Styleguide" />
+                        <h2>Post Pitch Redesign</h2>
+                            <h3>Initial Homepage Redesign</h3>
+                                <img src={initialRedesignImage} alt="Initial Homepage Redesign" />
+                            <h3>Refinement Concepts</h3>
+                                <img src={refinementConcepts} alt="Refinement Concepts" />
+                    </div>
+                    <div className='development-slice'>
+                        <h2>Development Outcomes</h2>
+                            <h3>Pitch Product</h3>
+                                <p>This was the version of the site used within the pitch presentation in Week 8.</p>
+                                <img src={pitchOutcomeImageMobile1} alt="Mobile Outcome 1" />
+                                <img src={pitchOutcomeImageMobile2} alt="Mobile Outcome 2" />
+                                <img src={pitchOutcomeImageDesktop1} alt="Desktop Outcome 1" />
+                                <img src={pitchOutcomeImageDesktop2} alt="Desktop Outcome 2" />
+                    </div>
                 </div>
             </div>
-            <div className='content-slice'>
+            <div className='content-slice portfolio-info-slices'>
                 <div className='slice-content'>
                     <h1>Feedback</h1>
                 </div>
             </div>
-            <div className='content-slice'>
+            <div className='content-slice portfolio-info-slices'>
                 <div className='slice-content'>
                     <h1>Reflection</h1>
                 </div>
             </div>
-            <div className='content-slice'>
+            <div className='content-slice portfolio-info-slices'>
                 <div className='slice-content'>
                     <h1>Future Plan</h1>
                 </div>
